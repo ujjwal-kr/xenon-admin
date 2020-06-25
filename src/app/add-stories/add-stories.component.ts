3aef926cd4a620637bef4e059417417000af01ca
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-06-25 15:20:49 +0530

    add story component

--- src/app/add-stories/add-stories.component.ts
+++ src/app/add-stories/add-stories.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { StoriesService } from '../services/stories.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Story } from './story';
 
 @Component({
   selector: 'app-add-stories',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-stories.component.sass']
 })
 export class AddStoriesComponent implements OnInit {
-
-  constructor() { }
+password: any;
+form: FormGroup;
+  constructor(
+    private authService: AuthService,
+    private storiesService: StoriesService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      title: ['', [
+        Validators.required
+      ]],
+      description: ['', [
+        Validators.required
+      ]],
+      image: [''],
+      url: ['']
+    })
+    this.password = localStorage.getItem("password")
+    this.authService.signIn(this.password);
+  }
+
+  get description() { return this.form.get('description').value }
+  get title() { return this.form.get('title').value }
+  get image() { return this.form.get('image').value }
+  get url() { return this.form.get('url').value }
+
+  submit() {
+    if (!this.form.errors){
+      const data: Story = {
+        title: this.title,
+        description: this.description,
+        image: this.image,
+        url: this.url
+      }
+      this.form.reset()
+      return this.storiesService.addStory(data);
+    }
+    return
   }
 
 }
